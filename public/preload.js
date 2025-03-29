48794e09abeb7181787d89152e8be347f6f636b1
凌览 <lihai_js@163.com>
2025-03-29 21:44:35 +0800

    refactor(preload.js): 移除自动备份功能的调用
    移除对 `autoBackup` 的调用，因为该功能暂时不需要或存在潜在问题

--- public/preload.js
+++ public/preload.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto')
 const bcrypt = require('./bcrypt/bcrypt.js')
-const { autoBackup } = require('./backup.js')
+// const { autoBackup } = require('./backup.js')
 const getKeyIv = (passphrase) => {
   const hash1 = crypto.createHash('md5').update(passphrase).digest('hex')
   const hash2 = crypto.createHash('md5').update(hash1 + passphrase).digest('hex')
@@ -88,7 +88,7 @@ window.services = {
   },
 
   // 自动备份数据
-  autoBackup,
+  // autoBackup,
   // 获取原始密码
   getOriginalPassword: () => {
     const passDoc = window.utools.db.get('bcryptpass')
